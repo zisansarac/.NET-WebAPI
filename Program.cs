006d3250855429c2fbe4f7d498666e2878e4df0e
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-10-18 17:07:16 +0300

    updated AddAuthentication

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using first_.NET_project.Data;
 using first_.NET_project.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 // burada web uygulamasını başlat demiş olduk.
@@ -25,3 +28,34 @@ builder.Services.AddIdentityCore<AppUser>(
         options.Password.RequireLowercase = false;
     }
 ).AddEntityFrameworkStores<AppDbContext>().AddSignInManager();
+//giriş sistemini ayarladık ve sonra verileri sakladık, ve girişi sağlayan otomatik fonksiyonu ekledik.
+
+//JWT ayarları burada yapılacak.
+var jwt = builder.Configuration.GetSection("JWTSettings");
+
+//key i alıp byte dizisine çeviriyoruz.
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
+
+//giriş sistemini jwt ye göre yapıyoruz.
+//varsayılan kimlik doğrulamasını jwt ile yap. Yani token le yap. Biz cookie ile değil token ile doğrulama yapacağız.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    options.DefaultChallengeScheme
+    = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwt["Issuer"],
+        ValidAudience = jwt["Audience"],
+        IssuerSigningKey = key,
+        ClockSkew = TimeSpan.Zero
+    };
+});
