08fc7f05f3f044afc49831352527e40af13b99f2
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-10-18 16:48:46 +0300

    added AddIdentityCore

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using first_.NET_project.Data;
+using first_.NET_project.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,3 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("ZisanConnection")));
 // burada veritabanı bağlantısını sağladık.
+// dedik ki bizim veri tabanı bağlantımız b, sen veri tabanını oluşturacaksın adına app.db diyeceksin. Oluşturduğun veri tabanının bilgileri AppDbContext'te olacak dedik.
+
+builder.Services.AddIdentityCore<AppUser>(
+    options =>
+    {
+        options.User.RequireUniqueEmail = true;
+        //her emailden sadece bir tane olmak zorunda.
+        options.Password.RequiredLength = 6;
+        //minimum şifre uzunluğu 6 karakter olacak.
+        options.Password.RequireNonAlphanumeric = false;
+        //şifre içerisinde özel karakterler zorunlu olmasın.
+        options.Password.RequireDigit = false;
+        //şifre içerisinde rakam zorunlu olmasın.
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    }
+);
